fbfda3b92ce3ba364ff41821ab94a7064886a110
Habib Manzur <habib@CO-IT006151.globant.com>
2020-03-04 00:06:08 -0500

    adding terraform and some ci scripts

---
+++ terraform.tf
@@ -0,0 +1,54 @@
+provider "aws" {}
+
+resource "aws_instance" "application" {
+  # https://cloud-images.ubuntu.com/locator/ec2/
+  ami = "ami-07ebfd5b3428b6f4d"
+  instance_type = "t2.micro"
+  key_name = "gorilla_ec2_key"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+  tags = {
+    Name = "Gorilla Test"
+  }
+}
+
+#resource "aws_key_pair" "deployer" {
+#  key_name   = "gorilla-key"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
+#}
+
+resource "aws_security_group" "instance" {
+  name        = "gorilla"
+  description = "SG for Gorilla test app"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.application.public_ip}"
+}
